08054ee50525626015561bc40b20d38c18951b22
newtask <irina.fedorova31@gmail.com>
2022-10-30 16:29:56 +0300

    new branch

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-     'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
-    'django.contrib.staticfilesblogapp'
-    'django.contrib.staticfilesdjango'
+    'django.contrib.staticfiles',
+    'django.contrib.staticfilesblogapp',
+    'django.contrib.staticfilesdjango',
     'blogapp.management.commands',
     'blogapp'
 ]
--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def main_view(request):
+    pass
+    render(request,"blogapp/index.html", context={} )
